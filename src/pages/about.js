208e368406db7a0b9c250919e6afb201e904ec74
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-04-13 12:31:05 +0200

    Contribution of about page

---
+++ src/pages/about.js
@@ -0,0 +1,96 @@
+import Head from "next/head";
+import AnimatedText from "../components/AnimatedText";
+import Layout from "../components/Layout";
+import Image from "next/image";
+import ProfilePicture from "../img/photo_Alex_R.jpg";
+import { useEffect, useRef } from "react";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+
+const AnimatedNumbers = ({ value }) => {
+    const ref = useRef(null);
+    const motionValue = useMotionValue(0);
+    const springValue = useSpring(motionValue, { duration: 3000 });
+    const isInView = useInView(ref, { once: true });
+
+    useEffect(() => {
+        if (isInView) {
+            motionValue.set(value);
+        }
+    }, [isInView, value, motionValue]);
+
+    useEffect(() => {
+        springValue.on("change", (latest) => {
+            if (ref.current && latest.toFixed(0) <= value) {
+                ref.current.textContent = latest.toFixed(0);
+            }
+        });
+    }, [springValue, value]);
+    return <span ref={ref}></span>;
+};
+
+const experienceYears = () => {
+    const currentYear = new Date().getFullYear();
+    const experienceYears = currentYear - 2019;
+    return experienceYears;
+};
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Forerunner | About Page</title>
+                <meta name="description" content="About page" />
+            </Head>
+            <main className="flex w-full flex-col items-center justify-center">
+                <Layout className="pt-16">
+                    <AnimatedText text="test" className="mb-16" />
+                    <div className="grid w-full grid-cols-8 gap-16">
+                        <div className="col-span-3 flex flex-col items-start justify-start">
+                            <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                                Biographie
+                            </h2>
+                            <p className="font-medium"></p>
+                            <p className="font-medium my-4"></p>
+                            <p className="font-medium"></p>
+                        </div>
+                        <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
+                            <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[102%] rounded-[2rem] bg-dark" />
+                            <Image
+                                src={ProfilePicture}
+                                alt="Real picture photo"
+                                className="w-full h-auto rounded-2xl"
+                            />
+                        </div>
+                        <div className="col-span-2 flex flex-col items-end justify-between">
+                            <div className="flex flex-col items-end justify-center">
+                                <span className="inline-block text-7xl font-bold">
+                                    <AnimatedNumbers value={10} /> +
+                                </span>
+                                <h2 className="text-xl font-medium capitalize text-dark/75">
+                                    Test 1
+                                </h2>
+                            </div>
+                            <div className="flex flex-col items-end justify-center">
+                                <span className="inline-block text-7xl font-bold">
+                                    <AnimatedNumbers value={20} /> +
+                                </span>
+                                <h2 className="text-xl font-medium capitalize text-dark/75">
+                                    Test 2
+                                </h2>
+                            </div>
+                            <div className="flex flex-col items-end justify-center">
+                                <span className="inline-block text-7xl font-bold">
+                                    <AnimatedNumbers value={experienceYears()} /> +
+                                </span>
+                                <h2 className="text-xl font-medium capitalize text-dark/75">
+                                    years of experience
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                </Layout>
+            </main>
+        </>
+    );
+};
+export default About;
